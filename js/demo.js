48f1d72e96a603aaf0b2fdc1a1fcad96241c9d8e
Tim Kim <tim.kim@nitobi.com>
2010-08-26 11:40:31 -0700

    Finished up demo

--- js/demo.js
+++ js/demo.js
@@ -1,8 +1,29 @@
 x$(window).on('load', function() {
-    try {
+		this.index = 0;
+		this.theTextbox = x$('#theTextbox')[0];
 		this.storage = new Lawnchair({'adaptor':'webkit'});
-    } catch(e) {
-        alert('Exception in load! ' + e);
-    }
+		//this.storage = new Lawnchair({'adaptor':'dom'});
+		//this.storage = new Lawnchair({'adaptor':'cookie'});
+		
+		x$('#saveButton').click(function()
+		{
+			console.log('Saving key as: ' + 'myKey'+index + ', with value: ' + theTextbox.value);
+			storage.save({key:'myKey'+index, value: theTextbox.value}, function(r){console.log(r)}); 
+			index++;
+		});
+		
+		x$('#getButton').click(function()
+		{
+			console.log('Retrieving everything in our store');
+			storage.each(function(r){console.log(r)}); 
+		});
+		
+		x$('#nukeButton').click(function()
+		{
+			console.log("Nuke it from orbit. It's the only way to be sure."); 
+			storage.nuke(); 
+			index = 0;
+		});
+
 });
 
